b30d8125080761e7b844e2511bd3c0f3ca087fee
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-23 13:06:39 -0300

    Adicionando informações em tela

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,17 @@
 import {Button} from './components/Button'
 import {ButtonCounter} from './components/ButtonCounter'
 
+import {Home} from './pages/Home'
+
 function App() {
 
 
 
   return (
-   <div>
-     <h1>Hello world!</h1>
-     <Button children="Botao 1"/>
-     <Button/>
-     <Button>Clique aqui</Button>
-     
-     <ButtonCounter></ButtonCounter>
 
+    <Home/>
 
-   </div>
+   
   );
 }
 
