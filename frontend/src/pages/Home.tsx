8101554d825894919ee6efea890d2747600588eb
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-02-23 15:14:33 -0300

    estilização home done

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,33 +2,36 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleImg from '../assets/images/google-icon.svg'
 
+import '../styles/auth.scss'
+
 
 export function Home(){
     return(
-        <div>
+        <div id='page-auth'>
             <aside>
 
                 <img src={illustrationImg} alt="ilustracao troca perguntas e respostas"/>
                 <strong>Crie salas de Q&amp;A ao vivo</strong>
                 <p>Tire as duvidas da sua audiencia em tempo real  </p>
+               
                 
             </aside>
 
             <main>
-                <div>
+                <div className='main-content'>
                     <img src={logoImg} alt="imagem de logo letmeask" />
-                    <button>
+                    <button className='btn'>
                         <img src={googleImg} alt="" />
                         Crie sua sala com o google
 
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className='separator'>ou entre em uma sala</div>
 
                     <form action="">
                         <input type="text" placeholder='digite o codigo da sala' name="" id="" />
                     </form>
 
-                    <button>Entrar na sala</button>
+                    <button className='btn'>Entrar na sala</button>
                 </div>
             </main>
 
